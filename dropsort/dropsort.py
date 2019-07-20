3def98f07ff1860d5c4a68aef981197535ce2333
prairir <prairie421@gmail.com>
2019-07-19 22:59:08 -0400

    forgot to get rid of testing lines

--- dropsort/dropsort.py
+++ dropsort/dropsort.py
@@ -4,9 +4,6 @@ def dropsort(array):
     i = 0
     #pythons for loops dont have true conditionals and so we are using a while loop to substitute
     while(i < length):
-        print("wall", wall)
-        print("value", array[i])
-        print("array", array)
         #checking if smaller than the wall
         if(array[i] < wall):
             del array[i]
@@ -20,4 +17,3 @@ def dropsort(array):
 
     return array
 
-print(dropsort([1,3,2,4,3,5,4]))
