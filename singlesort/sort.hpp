7e88d009cd1ba1ddb786652773f683120f57f863
Connor Horman <chorman64@gmail.com>
2019-12-10 22:46:16 -0500

    Update sort.hpp

--- singlesort/sort.hpp
+++ singlesort/sort.hpp
@@ -9,7 +9,7 @@
 * Expects: [begin,end) contains at most 1 element.
 */
 template<typename ForwardIterator,typename=std::enable_if_t<std::is_base_of_v<std::forward_iterator_tag,typename std::iterator_traits<ForwardIterator>::iterator_category>>>
-    ForwardIterator single_sort(ForwardIterator begin,ForwardIterator end) noexcept(noexcept(*begin < *begin)){
+    constexpr ForwardIterator single_sort(ForwardIterator begin,ForwardIterator end) noexcept(noexcept(*begin < *begin)){
   assert(std::distance(begin,end)<2);
   return begin;
 }
@@ -19,7 +19,7 @@ template<typename ForwardIterator,typename=std::enable_if_t<std::is_base_of_v<st
 * Expects: [begin,end) contains at most 1 element, and Predicate shall be a comparator function that establishes total order of elements of the range.
 */
 template<typename ForwardIterator,typename Predicate,typename=std::enable_if_t<std::is_base_of_v<std::forward_iterator_tag,typename std::iterator_traits<ForwardIterator>::iterator_category>>>
-    ForwardIterator single_sort(ForwardIterator begin,ForwardIterator end,Predicate&& pred) noexcept(noexcept(pred(*begin,*begin)){
+    constexpr ForwardIterator single_sort(ForwardIterator begin,ForwardIterator end,Predicate&& pred) noexcept(noexcept(pred(*begin,*begin)){
   assert(std::distance(begin,end)<2);
   return begin;
 }
