506b1103ae9ef2eab5220c3deedefbd07d74e59b
Connor Horman <chorman64@gmail.com>
2019-12-10 22:39:13 -0500

    Update and rename specialsort/sort.hpp to singlesort/sort.hpp

--- singlesort/sort.hpp
+++ singlesort/sort.hpp
@@ -5,7 +5,7 @@
 #include <type_traits>
 
 template<typename ForwardIterator,typename=std::enable_if_t<std::is_base_of_v<std::forward_iterator_tag,typename std::iterator_traits<ForwardIterator>::iterator_category>>>
-    ForwardIterator special_sort(ForwardIterator begin,ForwardIterator end){
+    ForwardIterator single_sort(ForwardIterator begin,ForwardIterator end){
   assert(std::distance(begin,end)<2);
   return begin;
 }
